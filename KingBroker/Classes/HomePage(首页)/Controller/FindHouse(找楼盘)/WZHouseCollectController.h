08fe44455bae54653191db574af44c9f5db51479
朱玉隆 <zhuyulong@zhuyulongdemacbook-pro.local>
2018-07-16 21:54:02 +0800

    1.2

---
+++ KingBroker/Classes/HomePage(首页)/Controller/FindHouse(找楼盘)/WZHouseCollectController.h
@@ -0,0 +1,13 @@
+//
+//  WZHouseCollectController.h
+//  KingBroker
+//
+//  Created by 朱玉隆 on 2018/7/5.
+//  Copyright © 2018年 朱玉隆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WZHouseCollectController : UIViewController
+-(void)loadRefreshs;
+@end
