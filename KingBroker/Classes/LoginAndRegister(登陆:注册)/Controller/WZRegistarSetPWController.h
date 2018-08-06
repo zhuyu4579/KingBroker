c323420cddca7a0c9c990102c239d416c58bd9e3
朱玉隆 <zhuyulong@zhuyulongdemacbook-pro.local>
2018-08-06 10:05:11 +0800

    注册设置密码

---
+++ KingBroker/Classes/LoginAndRegister(登陆:注册)/Controller/WZRegistarSetPWController.h
@@ -0,0 +1,13 @@
+//
+//  WZRegistarSetPWController.h
+//  KingBroker
+//
+//  Created by 朱玉隆 on 2018/8/6.
+//  Copyright © 2018年 朱玉隆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WZRegistarSetPWController : UIViewController
+
+@end
