34d93ca27460595ab7d0799f54bc8580377f8813
朱玉隆 <zhuyulong@zhuyulongdemacbook-pro.local>
2018-08-15 09:59:36 +0800

    创建新报备页面

---
+++ KingBroker/Classes/HomePage(首页)/Controller/Report(报备)/WZNewReportController.m
@@ -0,0 +1,37 @@
+//
+//  WZNewReportController.m
+//  KingBroker
+//
+//  Created by 朱玉隆 on 2018/8/15.
+//  Copyright © 2018年 朱玉隆. All rights reserved.
+//
+
+#import "WZNewReportController.h"
+
+@interface WZNewReportController ()
+
+@end
+
+@implementation WZNewReportController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
